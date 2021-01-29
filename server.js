42ea29d1d3491acbba6f7a90534c1b3d92e610cd
Justin Young <jyoung7223@yahoo.com>
2021-01-29 16:47:12 -0700

    Added indexedDB offline functions

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const Path = require("path");
+const Express = require("express");
+const Logger = require("morgan");
+const Mongoose = require("mongoose");
+const Compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || "dev";
+const App = Express();
 
-const app = express();
+App.use(Logger(NODE_ENV));
 
-app.use(logger("dev"));
+App.use(Compression());
+App.use(Express.urlencoded({ extended: true }));
+App.use(Express.json());
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+App.use(Express.static(Path.join(__dirname, "public")));
 
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+Mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
-app.use(require("./routes/api.js"));
+App.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+App.listen(PORT, () => {
+  console.log(`App running on port ${PORT} in ${NODE_ENV} mode!`);
 });
